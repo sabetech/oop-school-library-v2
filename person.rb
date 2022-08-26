917022b9697997946676ad74b21c16a34b5f893a
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-08-26 10:02:32 +0000

    Set up oop associations

--- person.rb
+++ person.rb
@@ -1,16 +1,29 @@
-class Person
-  attr_reader :id
+require './nameable'
+require './rental'
+class Person < Nameable
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
     (of_age? || parent_permission?)
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rentals=(rental)
+    rental.person = self
+    rentals.push(rental)
   end
 
   private
